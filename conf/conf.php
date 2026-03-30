8c5b944eb71974303a9144fe2bd62aa044868e82
garniik <antoine.arnoultfr@gmail.com>
2026-03-30 10:34:57 +0200

    structure de base

---
+++ conf/conf.php
@@ -0,0 +1,7 @@
+<?php
+$parameters['host'] = 'localhost';
+$parameters['port'] = '8099';
+$parameters['username'] = 'fakeuser';
+$parameters['password'] = 'fakepass';
+$parameters['dbname'] = 'fakedbname';
+$parameters['dbprefix'] = 'pdb_';
