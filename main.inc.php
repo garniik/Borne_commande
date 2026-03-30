d8aee640a4562f34e7e56e0656d4e6cd78c8ebde
garniik <antoine.arnoultfr@gmail.com>
2026-03-30 15:59:09 +0200

    fix:correction BDD

--- main.inc.php
+++ main.inc.php
@@ -1,10 +1,10 @@
 <?php
-include dirname(__FILE__) . "/public/inc/head.php";
+include dirname(__FILE__) . "/inc/head.php";
 ?>
 <div class="maincontent  w3-display-container w3-center">
     <?php
-        include dirname(__FILE__) . "/public/inc/content.php";
+        include dirname(__FILE__) . "/inc/content.php";
     ?>
 </div>
 <?php
-include dirname(__FILE__) . "/public/inc/footer.php";
+include dirname(__FILE__) . "/inc/footer.php";
